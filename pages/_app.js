451afdd9b0b26012b786042afa7322a3e9bfe712
Iqbal Wahyudi <iqbalwahyudi@pusri.co.id>
2024-01-30 09:23:31 +0700

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import '@/styles/styles.css'
+import '@/styles/custom.css'
+import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
